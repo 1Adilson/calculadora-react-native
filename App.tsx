be642ed0ff157f3c3fa55d5f779a942ac5ec0e01
Adilson Junior <juniorkadosh12@gmail.com>
2021-06-08 13:19:31 +0000

    feat(mobile): Adiciona botoes e Grid Collum.

--- App.tsx
+++ App.tsx
@@ -3,39 +3,89 @@ import Constants from 'expo-constants';
 import React from 'react';
 import {ViewYellow, Title, InputCal, ButtonCal } from "./style"
 import { Formik } from 'formik';
-import { Alert, View, Button } from 'react-native';
-
+import { Alert, Text } from 'react-native';
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { View } from 'native-base';
 
 export default function App() {
   return (
     <ViewYellow style={{paddingTop: Constants.statusBarHeight}}>
       <StatusBar style="auto" />
       <Title>CALCULADORA</Title>
+     
       <Formik
         initialValues={{
           firstName: 0,
         }}
         onSubmit={(
           values: any,
-        ) => {
-          alert(values);
+          ) => {
+            alert(values);
+          }
         }
-      }
-      >
+        >
         {props => (
-          <View>
-            <InputCal/>
-            <ButtonCal color="black" title="1" onPress={() => Alert.alert("AQUI")}/>
-            <ButtonCal color="black" title="2" onPress={() => Alert.alert("AQUI")}/>
-            <ButtonCal color="black" title="3" onPress={() => Alert.alert("AQUI")}/>
-            <ButtonCal  title="4" onPress={() => Alert.alert("AQUI")}/>
-            <ButtonCal color="black" title="5" onPress={() => Alert.alert("AQUI")}/>
-            <ButtonCal  title="6" onPress={() => Alert.alert("AQUI")}/>
+          <View> 
+          <InputCal/>
+          <Grid>
+          <Col style={{ height: 400 }}>
+            <Row style={{ height: 100 }}>
+              <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>7</Text></ButtonCal>
+            </Row>
+            <Row style={{ height: 100 }}>
+              <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>4</Text></ButtonCal>
+            </Row>
+            <Row style={{  height: 100 }}>
+              <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>1</Text></ButtonCal>
+            </Row>
+          </Col>
+
+          <Col style={{  height: 400 }}>
+            <Row style={{ height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>8</Text></ButtonCal>
+            </Row>
+            <Row style={{ height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>5</Text></ButtonCal>
+            </Row>
+            <Row style={{height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>2</Text></ButtonCal>
+            </Row>
+            <Row style={{ height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>0</Text></ButtonCal>
+            </Row>
+          </Col>
+
+          <Col style={{ height: 400 }}>
+            <Row style={{height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>9</Text></ButtonCal>
+            </Row>
+            <Row style={{ height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>6</Text></ButtonCal>
+            </Row>
+            <Row style={{  height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>3</Text></ButtonCal>
+            </Row>
+            <Row style={{  height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>,</Text></ButtonCal>
+            </Row>
+          </Col>
+
+          <Col style={{ height: 400 }}>
+            <Row style={{height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>x</Text></ButtonCal>
+            </Row>
+            <Row style={{ height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>-</Text></ButtonCal>
+            </Row>
+            <Row style={{  height: 100 }}>
+            <ButtonCal><Text style={{color: "#000", textAlign: "center"}}>+</Text></ButtonCal>
+            </Row>
+          </Col>
+          
+        </Grid>
           </View>
         )}
       </Formik>
     </ViewYellow>
   );
 }
-
-
--- style.tsx
+++ style.tsx
@@ -1,5 +1,6 @@
 
-import {View, Text, TextInput, Button } from 'react-native';
+import { Button } from "native-base";
+import {View, Text, TextInput} from 'react-native';
 import styled from "styled-components/native";
 
 export const ViewYellow = styled(View)`
@@ -11,16 +12,29 @@ export const Title = styled(Text)`
     top: 100px;
     color: black;
     text-align: center;
+    font-weight: bold;
+    font-size: 20px;
 `;
 
 export const InputCal = styled(TextInput)`
-  
-    background-color: gray;
-    color: #fff;
+    left: 50px;
+    top: 200px;
+    background-color: #fff;
+    color: black;
+    border-width: 1px;
+    border-color: black;
+    padding: 10px;
+    width: 300px;
 `;
 
 export const ButtonCal = styled(Button)`
-     background: transparent;
-    color: palevioletred;
-
+    width: 65px; 
+    height: 65px;
+    justify-content: center;
+    border-radius: 10px;
+    left: 20px;
+    top: 300px;
+    background-color: #fff;
+    border-width: 2px;
 `;
+
